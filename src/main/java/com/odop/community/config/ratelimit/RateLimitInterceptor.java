cf5c57e9dce535e7d59bbdc3b4e2adfd93480447
BenchPress200 <jms0538@naver.com>
2024-06-11 04:39:55 +0900

    feat: add rate-limit

---
+++ src/main/java/com/odop/community/config/ratelimit/RateLimitInterceptor.java
@@ -0,0 +1,27 @@
+package com.odop.community.config;
+
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class RateLimitInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private Bucket bucket;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (bucket.tryConsume(1)) {
+            return true;
+        } else {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.getWriter().write("Too many requests");
+            return false;
+        }
+    }
+}

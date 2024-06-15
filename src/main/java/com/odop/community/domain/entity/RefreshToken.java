cc2ea66f634e83879058b0af82be6505607e7d90
BenchPress200 <jms0538@naver.com>
2024-06-15 18:19:58 +0900

    feat: add entity of refresh token

---
+++ src/main/java/com/odop/community/domain/entity/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.odop.community.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="refresh_tokens")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "token", nullable = false, unique = true)
+    private String token;
+}
